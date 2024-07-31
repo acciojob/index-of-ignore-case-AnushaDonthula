6039ac788ad97f1a673eb1bc3ab215c94b9291ed
AnushaDonthula <162703733+AnushaDonthula@users.noreply.github.com>
2024-07-31 22:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	function indexOfIgnoreCase(str, subStr) {
+    // Convert both strings to lowercase
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
+    
+    // Find the index of the first occurrence of lowerSubStr in lowerStr
+    const index = lowerStr.indexOf(lowerSubStr);
+    
+    // Return the index or -1 if not found
+    return index;
+	
+	
 }
 
 // Please do not change the code below
